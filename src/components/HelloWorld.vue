ba43e1d8081fa76b9ec8be7532a380aeffc1bdc5
Arthur <arthur.mirzoyan@vocter.com>
2018-12-23 21:32:42 +0200

    components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>
+      <template v-if="mode === 'edit'">
+        Edit Your data
+      </template>
+      <template v-else>
+        Create data from scratch
+      </template>
+    </h1>
+
+    <input type="text" :value="data.title" />
+    <textarea cols="30" rows="10" :value="data.content" @change="changeContent"></textarea>
+
+    <button @click="updateData" v-if="mode === 'edit'">Edit</button>
+    <button @click="createData" v-else>Create</button>
+
+    <slot>default</slot>
   </div>
 </template>
 
@@ -8,7 +23,39 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    mode: String,
+    data: {
+      type: Object
+    },
+    func: Function
+  },
+
+  data () {
+    return {
+      internalData: {
+        id: undefined,
+        title: '',
+        content: ''
+      }
+    }
+  },
+
+  methods: {
+    changeContent ($event) {
+      this.$emit('update:content', $event.target.value)
+    },
+
+    createData () {
+      this.func()
+
+      // this.data.id = Math.random(1, 5)
+      // this.data.content = this.internalData.content
+    },
+
+    updateData () {
+      alert('updateData')
+    }
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>title: {{data.title}}</h1>
+    <p>content: {{data.content}}</p>
+
+    <HelloWorld msg="Edit Modal" mode="edit" :data="data" v-if="data.id" />
+    <HelloWorld msg="Create modal" mode="create" v-else :data="data" :func="() => this.alertMethod()" @update:content="onChangeContent">
+    	not default
+    </HelloWorld>
   </div>
 </template>
 
@@ -17,7 +23,27 @@ export default {
 
   data () {
   	return {
-  		
+  		data: {
+  			id: undefined,
+  			title: '',
+  			content: ''
+  		}
+  	}
+  },
+
+  methods: {
+  	onChangeContent (value) {
+  		var confirmTest = confirm('Confirm?')
+
+  		if (confirmTest) {
+  			this.data.content = 'parent ' + value
+  		} else {
+  			alert('NO!!!!!')
+  		}
+  	},
+
+  	alertMethod () {
+  		alert('alertMethod')
   	}
   }
 }
